eb4b7a22034d6fa0fead364562bd548116033e28
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-07-09 06:38:32 +0300

    (_.debounce) and (_.throttle)

---
+++ learn-js/js/lodash.js
@@ -0,0 +1,19 @@
+// --------- затормозить --------
+_.throttle;
+// ------------------------------
+const OutputRef = document.querySelector(".js-coords");
+let mouseMove = 0;
+
+const throttledOnMouseMove = _.throttle(onMouseMove, 500);
+window.addEventListener("mousemove", throttledOnMouseMove);
+
+function onMouseMove(event) {
+  mouseMove += 1;
+}
+
+// --- ждет пока закончишь печатать ---
+_.debounce;
+// ------------------------------------
+
+const inputRef = document.querySelector(".js-class");
+inputRef.addEventListener("input", _.debounce(oninputChange, 300));
