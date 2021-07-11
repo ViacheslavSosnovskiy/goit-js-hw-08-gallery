f0c2345c3c9cfe6b70b3d4ae7460c7bb9a2082c5
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-07-11 20:02:45 +0300

    hw done

---
+++ app.js
@@ -0,0 +1,133 @@
+const galleryItems = [
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+];
+
+const refs = {
+  listGallery: document.querySelector(".js-gallery"),
+  modalWindow: document.querySelector(".js-lightbox"),
+  closeModalWindow: document.querySelector('[data-action="close-lightbox"]'),
+  image: document.querySelector(".lightbox__image"),
+  overlay: document.querySelector(".lightbox__overlay"),
+};
+
+function createItemGallery(item) {
+  return item
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+    })
+    .join("");
+}
+
+const itemGallery = createItemGallery(galleryItems);
+refs.listGallery.insertAdjacentHTML("beforeend", itemGallery);
+
+refs.listGallery.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  refs.modalWindow.classList.toggle("is-open");
+  window.addEventListener("keydown", closeEsc);
+  refs.overlay.addEventListener("click", closeModal);
+
+  galleryItems.forEach(({ preview, original, description }) => {
+    if (event.target.src === preview) {
+      refs.image.src = `${original}`;
+      refs.image.alt = `${description}`;
+    }
+  });
+}
+
+refs.closeModalWindow.addEventListener("click", closeModal);
+
+function closeModal(event) {
+  refs.modalWindow.classList.toggle("is-open");
+  refs.image.src = "";
+  refs.image.alt = "";
+  window.removeEventListener("keydown", closeEsc);
+  refs.overlay.removeEventListener("click", closeModal);
+}
+
+function closeEsc(event) {
+  if (event.code === "Escape") {
+    closeModal();
+  }
+}
